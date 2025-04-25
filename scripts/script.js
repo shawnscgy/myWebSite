823410b1bd57bb2c14c2bbeaca573ac529b87f93
shawnscgy <166419310+shawnscgy@users.noreply.github.com>
2025-04-25 13:11:04 -0600

    4.25

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,56 @@
-let wholePage = document.getElementById("main");
+let wholePage = document.getElementById('main');
 let originalPage = wholePage.innerHTML;
-let sumHeaderElement = document.getElementById("sumHeader");
-sumHeaderElement.classList.add("tooClose");
+let sumHeaderElement = document.getElementById('sumHeader');
+//sumHeaderElement.classList.add("tooClose");
 
-let sumText = document.getElementById("sumText");
-let skiText = document.getElementById("skiText");
-let expText = document.getElementById("expText");
-let eduText = document.getElementById("eduText");
+let sumText = document.getElementById('sumText');
+let skiText = document.getElementById('skiText');
+let expText = document.getElementById('expText');
+let eduText = document.getElementById('eduText');
+let proText = document.getElementById('proText');
 
-let footerElement = document.getElementById("footer");
+let footerElement = document.getElementById('footer');
 
 // Summary
-let sumElement = document.getElementById("sum");
-sumElement.addEventListener("click", turnToSum);
+let sumElement = document.getElementById('sum');
+sumElement.addEventListener('click', turnToSum);
 function turnToSum() {
   wholePage.innerHTML = sumText.innerHTML;
 }
 
 // Skills
-let skiElement = document.getElementById("ski");
-skiElement.addEventListener("click", turnToSki);
+let skiElement = document.getElementById('ski');
+skiElement.addEventListener('click', turnToSki);
 function turnToSki() {
-  footerElement.style.marginTop = "20px";
+  footerElement.style.marginTop = '20px';
   wholePage.innerHTML = skiText.innerHTML;
 }
 
 // Experience
-let expElement = document.getElementById("exp");
-expElement.addEventListener("click", turnToExp);
+let expElement = document.getElementById('exp');
+expElement.addEventListener('click', turnToExp);
 function turnToExp() {
   wholePage.innerHTML = expText.innerHTML;
 }
 
 // Education
-let eduElement = document.getElementById("edu");
-eduElement.addEventListener("click", turnToEdu);
+let eduElement = document.getElementById('edu');
+eduElement.addEventListener('click', turnToEdu);
 function turnToEdu() {
   wholePage.innerHTML = eduText.innerHTML;
 }
 
 // Main
-let maiElement = document.getElementById("mai");
-maiElement.addEventListener("click", turnToMai);
+let maiElement = document.getElementById('mai');
+maiElement.addEventListener('click', turnToMai);
 function turnToMai() {
-  footerElement.style.marginTop = "0px";
+  footerElement.style.marginTop = '0px';
   wholePage.innerHTML = originalPage;
+}
+
+// Projects
+let proElement = document.getElementById('pro');
+proElement.addEventListener('click', turnToPro);
+function turnToPro() {
+  wholePage.innerHTML = proText.innerHTML;
 }
